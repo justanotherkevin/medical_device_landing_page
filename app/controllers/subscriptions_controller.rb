ea9692e009095f1e9844f90baa592c8d5d96dc5f
kevin hu <hu.kevin.w@gmail.com>
2017-04-10 10:59:54 -0400

    Finish demo4

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,21 +2,22 @@ class SubscriptionsController < ApplicationController
   def index
     @subscription = Subscription.new
   end
-  # def new
-  #   @subscription = Subscription.new
-  # end
+  def new
+    @subscription = Subscription.new
+  end
   def create
     @subscription = Subscription.new(subscripton_params)
     if @subscription.save
       flash[:notice] = "Post successfully created"
       redirect_to root_url
     else
-      render 'new'
+      flash[:notice] = "Email already taken / Not a vaild email"
+      redirect_to root_url
     end
   end
 
   private
   def subscripton_params
-    params.require(:subscription).permit(:first_name, :last_name, :email)
+    params.require(:subscription).permit(:email)
   end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,3 @@
 class Subscription < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-
 end
