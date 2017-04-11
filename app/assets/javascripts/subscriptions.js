af3522d9faeb22861249c9a5d766e746f99c5107
kevin hu <hu.kevin.w@gmail.com>
2017-04-11 09:30:11 -0400

    Add pop-up delay

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -7,9 +7,9 @@ $(function() {
 
   $('.pop-up-button').click(function(){
     $('.pop-up-container').show(0);
-    $('.pop-up').fadeIn(300);
+    $('.pop-up').fadeIn(400);
     $('.pop-up-button').hide(0);
-    $('.first_panel').hide(0);
+    $('.first_panel').fadeOut(100);
     $('section.second_panel').css({'padding': '0px 0px', 'height': '600px'});
     $('section.second_panel div.dim_background').css({'background': 'none', 'background-radious': 'none'});
 
